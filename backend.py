779d423f059dc057046d44afd95da3cf43be54fb
Brenton Graham <23674@burnside.school.nz>
2026-05-26 09:25:37 +1200

    Making prgress with the account creation but it still wont add users

--- backend.py
+++ backend.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for, request
+from flask import Flask, render_template, session, redirect, url_for, request, flash
 from flaskwebgui import FlaskUI
 import sqlite3
 
@@ -35,6 +35,17 @@ def databaseControl(query, data, function, table):
                 return userData 
             except Exception as e:
                 print(f"An error has occured at function=databaseControl while trying to 'retrieve' data from 'user' table. {e}")
+    elif function == 'check':
+        if table == 'user':
+            try:
+                dbCursor.execute(query, (data[0],))
+                userData = dbCursor.fetchall()
+                dbConnection.close()
+                # Sends the retrieved data back to the login function
+                return userData 
+            except Exception as e:
+                print(f"An error has occured at function=databaseControl while trying to 'check' data from 'user' table. {e}")
+
     
 
 
@@ -83,15 +94,29 @@ def accountCreate():
             query = """SELECT * FROM user WHERE userName = ?;"""
             dataList = []
             dataList.append(usernameInput)
-            usernameExist = databaseControl(query, dataList, 'retrieve', 'user')
+            usernameExist = databaseControl(query, dataList, 'check', 'user')
             if len(usernameExist) > 0:
-                return render_template('accountCreate.html', error='Username already used. Maybe sign-in?')
-            elif len(usernameExist) < 1:
-                del dataList
+                flash()
+                return render_template('accountCreate.html')
+            else:
                 query = """SELECT * FROM user WHERE userEmail = ?;"""
+                del dataList
                 dataList = []
                 dataList.append(emailInput)
-                emailExist = databaseControl(query, dataList, 'retrieve', 'user')
+                emailExist = databaseControl(query, dataList, 'check', 'user')
+                if len(emailExist) > 0:
+                    return render_template('accountCreate.html', error='Email already in use! Try again.')
+                else:
+                    query = """INSERT INTO user (userName, userPassword, userEmail) VALUES (?, ?, ?);"""
+                    del dataList
+                    dataList = []
+                    dataList.append(usernameInput)
+                    dataList.append(passwordInput)
+                    dataList.append(emailInput)
+                    databaseControl(query, dataList, 'add', 'user')
+                    return redirect(url_for('login'))
+        return render_template('accountCreate.html', error='One of the credentials are not the correct length. 5 to 20 characters!')
+
             
         usernameInput = request.form['username']
         passwordInput = request.form['password']
