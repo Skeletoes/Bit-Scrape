2499fd59abbbd34eb8bfb9b0caa11c10820978a9
Brenton Graham <23674@burnside.school.nz>
2026-05-28 15:26:52 +1200

    Login and account creation finally work

--- backend.py
+++ backend.py
@@ -14,9 +14,6 @@ def db(query, params=()):
     conn.close()
     return results
 
-    
-
-
 
 @app.route('/')
 def index():
@@ -31,50 +28,36 @@ def login():
         session['userName'] = request.form['username']
         session['userPassword'] = request.form['password']
 
+        # Check if the user credentials are in the database
         userData = db('SELECT * FROM user WHERE userName = ? AND userPassword = ?;', (session['userName'], session['userPassword']))
         if not userData:
-            # No users found
+            flash('Credentials are incorrect')
         else:
-            # User found
-            return render_template('login.html')
+            return redirect(url_for('home'))
+    return render_template('login.html')
 
 @app.route('/accountCreate', methods=['POST', 'GET'])
 def accountCreate():
     if request.method == 'POST':
+        # Get the credentials the the user input from the form
         usernameInput = request.form['username']
         passwordInput = request.form['password']
         emailInput = request.form['email']
-        if 5 < len(usernameInput) < 20 and 5 < len(passwordInput) < 20 and 5 < len(emailInput) < 20:
-            query = """SELECT * FROM user WHERE userName = ?;"""
-            dataList = []
-            dataList.append(usernameInput)
-            usernameExist = databaseControl(query, dataList, 'check', 'user')
-            if len(usernameExist) > 0:
-                flash()
-                return render_template('accountCreate.html')
+        
+        # Check the length of the username and password
+        if 5 < len(usernameInput) < 20 and 5 < len(passwordInput) < 20:
+            # Check if the username or email is already in the database
+            userCheck = db("SELECT * FROM user WHERE userName = ? OR userEmail = ?;", (usernameInput, emailInput))
+            # If the username or email is not in the database then add the user credentials
+            if not userCheck:
+                db("INSERT INTO user (userName, userPassword, userEmail) VALUES (?, ?, ?);", (usernameInput, passwordInput, emailInput))
+                # Redirect the user to the login page so they can login with their newly created account
+                return redirect(url_for('login'))
             else:
-                query = """SELECT * FROM user WHERE userEmail = ?;"""
-                del dataList
-                dataList = []
-                dataList.append(emailInput)
-                emailExist = databaseControl(query, dataList, 'check', 'user')
-                if len(emailExist) > 0:
-                    return render_template('accountCreate.html', error='Email already in use! Try again.')
-                else:
-                    query = """INSERT INTO user (userName, userPassword, userEmail) VALUES (?, ?, ?);"""
-                    del dataList
-                    dataList = []
-                    dataList.append(usernameInput)
-                    dataList.append(passwordInput)
-                    dataList.append(emailInput)
-                    databaseControl(query, dataList, 'add', 'user')
-                    return redirect(url_for('login'))
-        return render_template('accountCreate.html', error='One of the credentials are not the correct length. 5 to 20 characters!')
+                flash('That username or email is already being used.')
+        else:
+            flash('The username of password is not long enough. (5 to 20 characters!!!)')
 
-            
-        usernameInput = request.form['username']
-        passwordInput = request.form['password']
-        emailInput = request.form['email']
     return render_template('accountCreate.html')
 
 
@@ -83,9 +66,6 @@ def accountCreate():
 def home():
     return render_template('home.html')
 
-    
-
-    
 
 if __name__ == '__main__':
     FlaskUI(app=app, server="flask", width=800, height=480, port=8000).run()
