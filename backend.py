c887b9e355af1a075251bc1ecc01658a7fc6af22
Brenton Graham <23674@burnside.school.nz>
2026-08-07 07:33:57 +1200

    Tried to make the scraping agent better but i may have made is worse :(

--- backend.py
+++ backend.py
@@ -1,20 +1,46 @@
+# Essential libraries that make up the backend of the backend
 from flask import Flask, render_template, session, redirect, url_for, request, flash
 import sqlite3
 import webview
 from threading import Thread
-from playwright.sync_api import sync_playwright, expect
+import os
+
+# Very useful libraries for the little features of the app
 from PIL import Image
 import datetime
 import time
-import os
+
+# Libraries for sending emails
 import smtplib
 from email.mime.text import MIMEText
+
+# Libraries to improve the overall secureness of the app
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from dotenv import load_dotenv
 
+# Web scraper libraries
+from crawlee.crawlers import PlaywrightCrawler, PlaywrightCrawlingContext
+from crawlee.storage_clients import MemoryStorageClient
+import asyncio
 
+
+# Link to the '.env' file
 load_dotenv()
+
+# One persistent event loop, shared by every scrape for the life of the app
+_background_loop = asyncio.new_event_loop()
+
+def _run_background_loop():
+    asyncio.set_event_loop(_background_loop)
+    _background_loop.run_forever()
+
+Thread(target=_run_background_loop, daemon=True).start()
+
+def run_async(coro):
+    """Submit a coroutine to the shared background loop from any sync thread."""
+    future = asyncio.run_coroutine_threadsafe(coro, _background_loop)
+    return future.result()
 
 
 app = Flask(__name__)
@@ -88,137 +114,143 @@ def login():
                 return redirect(url_for('home'))
         return render_template('login.html')
     except Exception as e:
-        
+        error(e)
 
 
 @app.route('/accountCreate', methods=['POST', 'GET'])
 def accountCreate():
-    if request.method == 'POST':
-        # Get the credentials the the user input from the form
-        usernameInput = request.form['username']
-        passwordInput = request.form['password']
-        emailInput = request.form['email']
-        
-        # Check the length of the username and password
-        if 5 < len(usernameInput) < 20 and 5 < len(passwordInput) < 20:
-            # Check if the username or email is already in the database
-            userCheck = db("SELECT * FROM user WHERE userName = ? OR userEmail = ?;", (usernameInput, emailInput))
-            # If the username or email is not in the database then add the user credentials
-            if not userCheck:
-                hashedPassword = generate_password_hash(passwordInput) # Generate a password hash to keep the users password is secure
-                db("INSERT INTO user (userName, userPassword, userEmail) VALUES (?, ?, ?);", (usernameInput, hashedPassword, emailInput))
-                # Redirect the user to the login page so they can login with their newly created account
-                return redirect(url_for('login'))
+    try:
+        if request.method == 'POST':
+            # Get the credentials the the user input from the form
+            usernameInput = request.form['username']
+            passwordInput = request.form['password']
+            emailInput = request.form['email']
+            
+            # Check the length of the username and password
+            if 5 < len(usernameInput) < 20 and 5 < len(passwordInput) < 20:
+                # Check if the username or email is already in the database
+                userCheck = db("SELECT * FROM user WHERE userName = ? OR userEmail = ?;", (usernameInput, emailInput))
+                # If the username or email is not in the database then add the user credentials
+                if not userCheck:
+                    hashedPassword = generate_password_hash(passwordInput) # Generate a password hash to keep the users password is secure
+                    db("INSERT INTO user (userName, userPassword, userEmail) VALUES (?, ?, ?);", (usernameInput, hashedPassword, emailInput))
+                    # Redirect the user to the login page so they can login with their newly created account
+                    return redirect(url_for('login'))
+                else:
+                    flash('That username or email is already being used.')
             else:
-                flash('That username or email is already being used.')
-        else:
-            flash('The username of password is not long enough. (5 to 20 characters!!!)')
-
-    return render_template('accountCreate.html')
+                flash('The username of password is not long enough. (5 to 20 characters!!!)')
+        return render_template('accountCreate.html')
+    except Exception as e:
+        error(e)
 
 
 @app.route('/home')
 @login_required
 def home():
-    scraperAgents = db("""SELECT scraperName FROM scraperAgent WHERE userID = ?;""", (session['userID'], ))
-    return render_template('home.html', agents=[row[0] for row in scraperAgents])
-
+    try:
+        scraperAgents = db("""SELECT scraperName FROM scraperAgent WHERE userID = ?;""", (session['userID'],))
+        return render_template('home.html', agents=[row[0] for row in scraperAgents])
+    except Exception as e:
+        error(e)
 
 @app.route('/agentSelect', methods=['POST', 'GET'])
 @login_required
 def agentSelect():
-    if request.method == 'POST':
-        agentName = request.form['agentName']
-        agent = db("""SELECT scraperID FROM scraperAgent WHERE userID = ? and scraperName = ?;""", (session['userID'], agentName,))
-        session['scraperID'] = agent[0][0]
-    return redirect(url_for('agentConfig'))
-
+    try:
+        if request.method == 'POST':
+            agentName = request.form['agentName']
+            agent = db("""SELECT scraperID FROM scraperAgent WHERE userID = ? and scraperName = ?;""", (session['userID'], agentName,))
+            session['scraperID'] = agent[0][0]
+        return redirect(url_for('agentConfig'))
+    except Exception as e:
+        error(e)
 
 @app.route('/agentDelete', methods=['POST', 'GET'])
 @login_required
 def agentDelete():
-    if request.method == 'POST':
-        db("""DELETE FROM scrapeData WHERE scraperID =?;""", (session['scraperID'],))
-        db("""DELETE FROM scraperAgent WHERE scraperID = ?;""", (session['scraperID'],))
-    return redirect(url_for('home'))
+    try:
+        if request.method == 'POST':
+            db("""DELETE FROM scrapeData WHERE scraperID =?;""", (session['scraperID'],))
+            db("""DELETE FROM scraperAgent WHERE scraperID = ?;""", (session['scraperID'],))
+        return redirect(url_for('home'))
+    except Exception as e:
+        error(e)
 
 
 @app.route('/userDelete', methods=['POST', 'GET'])
 @login_required
 def userDelete():
-    if request.method == 'POST':
-        db("""DELETE FROM scrapeData WHERE userID = ?;""", (session['userID'],))
-        db("""DELETE FROM scraperAgent WHERE userID = ?;""", (session['userID'],))
-        db("""DELETE FROM user WHERE userID = ?;""", (session['userID'],))
-    return redirect(url_for('index'))
-
+    try:
+        if request.method == 'POST':
+            db("""DELETE FROM scrapeData WHERE userID = ?;""", (session['userID'],))
+            db("""DELETE FROM scraperAgent WHERE userID = ?;""", (session['userID'],))
+            db("""DELETE FROM user WHERE userID = ?;""", (session['userID'],))
+        return redirect(url_for('index'))
+    except Exception as e:
+        error(e)
 
 @app.route('/agentConfig', methods=['POST', 'GET'])
 @login_required
 def agentConfig():
-    if request.method == 'POST':
-        userAgents_names = db("""SELECT scraperName FROM scraperAgent WHERE userID = ?;""", (session['userID'],))
-        userAgents_names = [row[0] for row in userAgents_names] # Loops through the tuples and creates a new easy to access list
-        scraperID = session['scraperID']
-        newAgent_name = request.form['newAgent-name']
-        newAgent_link = request.form['newAgent-link']
-        newAgent_selector = request.form['newAgent-selector']
-        scrapeInterval = request.form['scrapeInterval']
-        if newAgent_name:
-            if newAgent_name in userAgents_names:
-                flash("You have already used thet scraper agent name!")
+    try:
+        if request.method == 'POST':
+            userAgents_names = db("""SELECT scraperName FROM scraperAgent WHERE userID = ?;""", (session['userID'],))
+            userAgents_names = [row[0] for row in userAgents_names] # Loops through the tuples and creates a new easy to access list
+            scraperID = session['scraperID']
+            newAgent_name = request.form['newAgent-name']
+            newAgent_link = request.form['newAgent-link']
+            newAgent_selector = request.form['newAgent-selector']
+            scrapeInterval = request.form['scrapeInterval']
+            if newAgent_name:
+                if newAgent_name in userAgents_names:
+                    flash("You have already used thet scraper agent name!")
+                else:
+                    db("""UPDATE scraperAgent SET scraperName = ? WHERE scraperID = ?;""", (newAgent_name, scraperID,))
             else:
-                db("""UPDATE scraperAgent SET scraperName = ? WHERE scraperID = ?;""", (newAgent_name, scraperID,))
-        else:
-            print("Scraper Agent name was not changed.")
-        if newAgent_link:
-            db("""UPDATE scraperAgent SET webPageURL = ? WHERE scraperID = ?;""", (newAgent_link, scraperID,))
-        else:
-            print("Scraper agent link was not changed.")
-        if newAgent_selector:
-            db("""UPDATE scraperAgent SET elementSelector = ? WHERE scraperID = ?;""", (newAgent_selector, scraperID,))
-        else:
-            print("Scraper agent selector was not changed.")
-        if scrapeInterval:
-            db("""UPDATE scraperAgent SET scrapeInterval = ? WHERE userID = ?;""", (scrapeInterval, session['userID'],))
-        return redirect(url_for('home'))
-    agentDetails = db("""SELECT scraperName, webPageURL, elementSelector, scrapeInterval FROM scraperAgent WHERE scraperID = ?;""", (session['scraperID'],))
-    name, link, selector, interval = agentDetails[0]
-    return render_template('agentConfig.html', name=name, link=link, selector=selector, interval=interval)
+                print("Scraper Agent name was not changed.")
+            if newAgent_link:
+                db("""UPDATE scraperAgent SET webPageURL = ? WHERE scraperID = ?;""", (newAgent_link, scraperID,))
+            else:
+                print("Scraper agent link was not changed.")
+            if newAgent_selector:
+                db("""UPDATE scraperAgent SET elementSelector = ? WHERE scraperID = ?;""", (newAgent_selector, scraperID,))
+            else:
+                print("Scraper agent selector was not changed.")
+            if scrapeInterval:
+                db("""UPDATE scraperAgent SET scrapeInterval = ? WHERE userID = ?;""", (scrapeInterval, session['userID'],))
+            return redirect(url_for('home'))
+        agentDetails = db("""SELECT scraperName, webPageURL, elementSelector, scrapeInterval FROM scraperAgent WHERE scraperID = ?;""", (session['scraperID'],))
+        name, link, selector, interval = agentDetails[0]
+        return render_template('agentConfig.html', name=name, link=link, selector=selector, interval=interval)
+    except Exception as e:
+        error(e)
 
 
 @app.route('/agentCreate', methods=['POST', 'GET'])
 @login_required
 def agentCreate():
-    if request.method == 'POST':
-        agentName_input = request.form['agentName']
-        webpageLink_input = request.form['webpageLink']
-        elementSelector_input = request.form['elementSelector']
-        scrapeInterval = request.form['scrapeInterval']
-        with sync_playwright() as p:
+    try:
+        if request.method == 'POST':
+            agentName_input = request.form['agentName']
+            webpageLink_input = request.form['webpageLink']
+            elementSelector_input = request.form['elementSelector']
+            scrapeInterval = request.form['scrapeInterval']
             try:
-                browser = p.chromium.launch()
-                page = browser.new_page()
-                page.goto(webpageLink_input)                
-                try:
-                    page.wait_for_selector(f"{elementSelector_input}", timeout=10000)
-                except Exception as wait_err:
-                    print(f"Selector not found: {wait_err}")
-                # Give the screen shot a temporary name, will delete after the image is opened
-                page.screenshot(path="tmpImg.png")
-                price_element = page.locator(f"{elementSelector_input}")
-                price_text = price_element.text_content()
-                print(f"Current price: {price_text}")
-                browser.close()
+                price_text = run_async(
+                    scrape_selector(webpageLink_input, elementSelector_input, "tmpImg.png")
+                )
                 img = Image.open("tmpImg.png")
                 img.show()
-                # Delete the screenshot
                 os.remove("tmpImg.png")
-                if not db("""SELECT * FROM scraperAgent WHERE scraperName = ?;""", (agentName_input, )):
-                    db("""INSERT INTO scraperAgent (userID, scraperName, webPageURL, elementSelector, scrapeInterval) VALUES (?, ?, ?, ?, ?);""", (session['userID'], agentName_input, webpageLink_input, elementSelector_input, scrapeInterval))
-                    agentID = db("""SELECT scraperID FROM scraperAgent WHERE userID = ? AND scraperName = ?;""", (session['userID'], agentName_input))
-                    agentID = agentID[0][0]
-                    db("""INSERT INTO scrapeData (userID, scraperID, scrapeValue, scrapeTime, elementSelector) VALUES (?, ?, ?, ?, ?);""", (session['userID'], agentID, price_text, datetime.datetime.now(), elementSelector_input))
+
+                if not db("""SELECT * FROM scraperAgent WHERE scraperName = ?;""", (agentName_input,)):
+                    db("""INSERT INTO scraperAgent (userID, scraperName, webPageURL, elementSelector, scrapeInterval) VALUES (?, ?, ?, ?, ?);""",
+                       (session['userID'], agentName_input, webpageLink_input, elementSelector_input, scrapeInterval))
+                    agentID = db("""SELECT scraperID FROM scraperAgent WHERE userID = ? AND scraperName = ?;""",
+                                 (session['userID'], agentName_input))[0][0]
+                    db("""INSERT INTO scrapeData (userID, scraperID, scrapeValue, scrapeTime, elementSelector) VALUES (?, ?, ?, ?, ?);""",
+                       (session['userID'], agentID, price_text, datetime.datetime.now(), elementSelector_input))
                     automation_Thread(scrapeInterval, agentID, session['userID'])
                     return redirect(url_for('home'))
                 else:
@@ -226,7 +258,9 @@ def agentCreate():
             except Exception as e:
                 print(e)
                 flash("There was an error accessing that webpage")
-    return render_template('agentCreate.html')
+        return render_template('agentCreate.html')
+    except Exception as e:
+        error(e)
 
 
 @app.route('/configure', methods=['POST', 'GET'])
@@ -255,7 +289,7 @@ def configure():
             print(e)
     userDetails = db("""SELECT userName, userEmail FROM user WHERE userID = ?;""", (session['userID'],))
     name, email = userDetails[0]
-    return render_template('configuration.html', name=name email=email)
+    return render_template('configuration.html', name=name, email=email)
 
 
 def automation_Thread(interval, agentID, userID):
@@ -266,31 +300,17 @@ def automation_Thread(interval, agentID, userID):
 def automation_Time(interval, agentID, userID):
     agentData = db("""SELECT webPageURL, elementSelector FROM scraperAgent WHERE scraperID = ?;""", (agentID,))
     link, selector = agentData[0]
-    hours = int(interval) * 10 # Set the interval low for testing purposes
-    while True: # Start the loop to do the timely checks
-        scrapeValue = automation_Scrape(link, selector)
+    hours = max(int(interval) * 10, 10) # Never sleep less than 10 seconds between scrapes
+    while True:
+        try:
+            scrapeValue = run_async(scrape_selector(link, selector))
+        except Exception as e:
+            scrapeValue = str(e)
         prevVal = db("""SELECT scrapeValue FROM scrapeData WHERE scraperID = ? ORDER BY scrapeID DESC LIMIT 1;""", (agentID,))
-        db("""INSERT INTO scrapeData (userID, scraperID, scrapeTime, scrapeValue, elementSelector) VALUES (?, ?, ?, ?, ?);""", (userID, agentID, datetime.datetime.now(), scrapeValue, selector,))
+        db("""INSERT INTO scrapeData (userID, scraperID, scrapeTime, scrapeValue, elementSelector) VALUES (?, ?, ?, ?, ?);""",
+           (userID, agentID, datetime.datetime.now(), scrapeValue, selector,))
         automation_Email(prevVal[0][0], scrapeValue, agentID, userID)
         time.sleep(hours)
-
-
-def automation_Scrape(link, selector):
-    with sync_playwright() as p:
-        try:
-            browser = p.chromium.launch()
-            page = browser.new_page()
-            page.goto(link)                
-            try:
-                page.wait_for_selector(f"{selector}", timeout=10000)
-            except Exception as wait_err:
-                print(f"Selector not found: {wait_err}")
-            price_element = page.locator(f"{selector}")
-            price_text = price_element.text_content()
-            browser.close()
-            return(price_text)
-        except Exception as e:
-            return str(e)
 
 
 def automation_Email(prevVal, curVal, agentID, userID):
@@ -328,7 +348,33 @@ def automation_Email(prevVal, curVal, agentID, userID):
             print("Email sent successfully!")
         except Exception as e:
             print(f"Error: {e}")
-        
+
+# The function that will return a string. The 'async' meaning that it will pause when it comes across an 'await' and continue with something else while it waits
+async def scrape_selector(url: str, selector: str, screenshot_path: str | None = None) -> str: 
+    result: dict[str, str] = {}
+
+    async def request_handler(context: PlaywrightCrawlingContext) -> None:
+        try:
+            await context.page.wait_for_selector(selector, timeout=10000) # Wait for the selector to appear
+        except Exception as wait_err:
+            context.log.warning(f"Selector not found: {wait_err}")
+
+        if screenshot_path: # If the call for the main function includes a sreenshot link then a screenshot of the page during the scrape will be taken
+            await context.page.screenshot(path=screenshot_path)
+
+        element = context.page.locator(selector) # Locator object to reference the CSS locator of the element
+        result["value"] = await element.text_content() or "" # Interact with the webpage and read the data at the locator and value
+
+    crawler = PlaywrightCrawler( # Configure the crawler (scraper agent)
+        request_handler=request_handler,
+        max_requests_per_crawl=1,   # one page per call
+        max_request_retries=3,      # Crawlee retries failed navigations automatically
+        headless=True,
+        storage_client=MemoryStorageClient(),
+    )
+
+    await crawler.run([url]) # Actually start the scrape
+    return result.get("value", "")
 
 def run_flask():
     app.run(port=8000)
